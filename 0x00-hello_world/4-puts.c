14285815215f4ceadc9fee7e170a9a85da91f10c
Danielapori <danielsapori19@gmail.com>
2022-03-14 15:51:17 -0700

    Using Puts to print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *main = enter command
- *Return: 0 after printing command
- */
+*main - enter command
+*Return: 0 after printing command
+*/
 int main(void)
- {
+{
 puts("\" Programming is like building a multilingual puzzle");
 return (0);
- }
+}
