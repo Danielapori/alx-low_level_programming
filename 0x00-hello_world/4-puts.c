7f998ee8d0853ac147842f4155c2ffa4bc02f669
Danielapori <danielsapori19@gmail.com>
2022-03-14 10:30:33 -0700

    Print using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ *main = enter command
+ *Return: 0 after printing command
+ */
+int main(void)
+ {
+puts("\" Programming is like building a multilingual puzzle");
+return (0);
