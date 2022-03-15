7e8ebf22ce07f505f9cf50903d26c3ecde35919a
Danielapori <danielsapori19@gmail.com>
2022-03-14 20:45:14 -0700

    Print with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - Enter command
+*Print
+*Return: return 1 after program exe
+*/
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art");
+return (0);
+}
